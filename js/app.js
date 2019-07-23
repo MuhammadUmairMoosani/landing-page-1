ef2551cb3f330db4c06ee64b13238ffdb54d7964
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-07-23 17:18:43 +0530

    navbar and scroll functions

--- js/app.js
+++ js/app.js
@@ -1,57 +1,40 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+const sections = Array.from(document.getElementsByTagName('section'))
+const title = document.getElementById('title')
 
-/**
- * Define Global Variables
- * 
-*/
+function focusOnScroll () {
+  sections.forEach(section => section.classList.remove('section-active'))
+  if (isElementInViewport(title)) return
+  const activeSection = sections.find(section => isElementInViewport(section.childNodes[1].childNodes[1]))
+  activeSection && activeSection.classList.add('section-active')
+}
 
+window.onscroll = focusOnScroll
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+function isElementInViewport (element) {
+  const rect = element.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
 
+function renderNavbar () {
+  const nav = document.getElementById('navbar__list')
+  const homeLink = document.createElement('li')
+  homeLink.innerText = 'Home'
+  homeLink.className = 'menu__link'
+  homeLink.onclick = () => window.scrollTo({ top: 0 })
+  nav.appendChild(homeLink)
+  sections.forEach(section => {
+    if (!section.dataset || !section.dataset.nav) return
+    const item = document.createElement('li')
+    item.innerText = section.dataset.nav
+    item.className = 'menu__link'
+    item.onclick = () => section.scrollIntoView()
+    nav.appendChild(item)
+  })
+}
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+renderNavbar()
